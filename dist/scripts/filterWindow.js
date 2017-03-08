8227eea5bb88900cfba1fa73130caed327996803
Ryan Andonian <ryanandonian@gmail.com>
2017-03-08 01:27:36 -0800

    Cleans up the tracking so now it uses the unique IDs

--- dist/scripts/filterWindow.js
+++ dist/scripts/filterWindow.js
@@ -1,7 +1,7 @@
 // Left hand window - for filtering
 FIL_ITEMS_STARTING_Y = 20;
 FIL_LIST_HEADER_PADDING = 0;
-FIL_LIST_START_X = 5;
+FIL_LIST_START_X = 20;
 FIL_LIST_PADDING_Y = 12;
 FIL_LIST_HEADER2_PADDING_Y = 25;
 class FilterWindow extends BaseWindow {
@@ -12,18 +12,21 @@ class FilterWindow extends BaseWindow {
   }
   initFilters() {
     this.filters = [];
-    this.personSprites = [];
-    var idCtr = 1;
+    this.personSprites = {};
     for (var i = 0; i < gameObjects.length; i++) {
+      var personId = gameObjects[i].id;
       this.filters.push({
         'tracked': false,
-        'filterId': idCtr++,
-        'id': gameObjects[i].name
+        'id': personId,
+        'name': gameObjects[i].name
       });
       var personSprite = game.add.sprite(0, 0, 'nodes');
       personSprite.frame = gameObjects[i].sprite.frame;
-      this.personSprites.push(personSprite);
+      this.personSprites[personId] = personSprite;
     }
+    this.filters.sort(function(left, right) {
+      return left.id.localeCompare(right.id);
+    });
   }
   onPageUp(event) {
     this.itemListY = Utils.clamp(-(gameObjects.length - 30) * FIL_LIST_PADDING_Y, FIL_ITEMS_STARTING_Y, this.itemListY + FIL_LIST_PADDING_Y * 5);
@@ -42,7 +45,7 @@ class FilterWindow extends BaseWindow {
         this.filters[i].tracked = false;
         continue;
       }
-      if (this.filters[i].filterId == filterArg) {
+      if (this.filters[i].id == filterArg) {
         this.filters[i].tracked = !this.filters[i].tracked;
       }
     }
@@ -73,13 +76,14 @@ class FilterWindow extends BaseWindow {
     super.render();
     var filters = this.filters
     var itemListY = this.itemListY;
-    var spritePaddingX = 45;
+    var spritePaddingX = 0;
     for (var i = 0; i < filters.length; i++) {
       var currFilter = filters[i];
-      var str = sprintf("%s)[%s].....%s", ('0000' + currFilter.filterId).slice(-2), (currFilter.tracked ? 'X' : ' '), currFilter.id);
-      this.drawText(FIL_LIST_START_X, this.itemListY + i * FIL_LIST_PADDING_Y, str, 12, undefined, 'green');
-      this.personSprites[i].x = FIL_LIST_START_X + spritePaddingX;
-      this.personSprites[i].y = this.itemListY + (i + 1) * FIL_LIST_PADDING_Y;
+      var str = sprintf("%s [%s]=[%s]", currFilter.name, currFilter.id, (currFilter.tracked ? 'X' : ' '));
+      var color = (currFilter.tracked) ? 'rgb(220, 0, 0)' : 'green';
+      this.drawText(FIL_LIST_START_X, this.itemListY + i * FIL_LIST_PADDING_Y, str, 12, undefined, color);
+      this.personSprites[currFilter.id].x = spritePaddingX;
+      this.personSprites[currFilter.id].y = this.itemListY + (i + 1) * FIL_LIST_PADDING_Y;
     }
     // Some helpful text
     this.drawText(this.width - 40, 15, '[PgUp]', 12);
